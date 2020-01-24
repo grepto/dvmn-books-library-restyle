b001bfdbd15d7cbecbee5a7c12de60733e7d527f
Alexandr Popov <grepto@gmail.com>
2020-01-24 17:29:09 +0300

    make batch download

---
+++ src/app.py
@@ -0,0 +1,22 @@
+import json
+
+from book import download_book
+from category import get_books_urls
+
+
+def batch_download(book_limit: int):
+    urls = get_books_urls(book_limit)
+    books_contexts = [download_book(url) for url in urls]
+
+    return books_contexts
+
+
+def main(book_limit: int):
+    books = batch_download(book_limit)
+
+    with open('books.json', 'w') as json_file:
+        json.dump(books, json_file, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main(100)
