eaf307925118eb42fdb8fc82668e6194c1fa8e39
grepto <grepto@gmail.com>
2020-03-28 17:18:04 +0300

    add copying books and images to root folder

--- src/app.py
+++ src/app.py
@@ -31,6 +31,7 @@ def main():
                            args.book_path,
                            args.image_path,
                            args.category_url,
+                           args.root_path,
                            )
 
     with open(args.book_json, 'w') as json_file:
